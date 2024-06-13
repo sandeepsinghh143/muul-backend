3ec6c951e84be9bd4493c80624c99c3aa6ddf811
sandeepsinghh143 <sandeepsinghh143@gmail.com>
2024-06-13 12:17:15 +0530

    updated2

--- index.js
+++ index.js
@@ -10,7 +10,11 @@ const swaggerDocs = require("./swagger-docs.json");
 
 const app = express();
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: "*",
+  })
+);
 app.use("/user", userRoutes);
 app.use("/sale", salesRoutes);
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
