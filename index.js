a7cdae1735e7755437e335cd48d97eca49834ee4
sandeepsinghh143 <sandeepsinghh143@gmail.com>
2024-06-13 11:51:40 +0530

    updated

--- index.js
+++ index.js
@@ -5,12 +5,15 @@ require("./dbConnection");
 const PORT = process.env.PORT;
 const userRoutes = require("./routes/user");
 const salesRoutes = require("./routes/sale");
+const swaggerUi = require("swagger-ui-express");
+const swaggerDocs = require("./swagger-docs.json");
 
 const app = express();
 app.use(express.json());
 app.use(cors());
 app.use("/user", userRoutes);
 app.use("/sale", salesRoutes);
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
 
 app.listen(PORT, () => {
   console.log(`Server is live on PORT: ${PORT}`);
